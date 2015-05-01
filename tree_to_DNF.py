7ac596360694487b4a9f9fcf7d4b98b103ccb80b
Jeremy Foote <jdfoote1@gmail.com>
2015-05-01 11:32:42 -0500

    Hacky way to get the number of splits

--- tree_to_DNF.py
+++ tree_to_DNF.py
@@ -3,14 +3,18 @@ import argparse
 from decision_tree import is_leaf, getAttr
 
 def tree_to_disjunctive_normal(tree):
-    '''Takes a tree, and returns a string representing
-    the disjunctive normal representation of the tree'''
+    '''
+    Takes a tree, and returns a string representing
+    the disjunctive normal representation of the tree and
+    the number of splits in the tree.
+    '''
     conditions = []
-    def find_conditions(tree, curr_string = '('):
+    def find_conditions(tree, curr_string = '(', count=0):
         if is_leaf(tree):
             if tree['label'] == 1:
                 conditions.append(curr_string + ')')
         else:
+            find_conditions.count += 1
             attr = getAttr(tree)
             for x in tree[attr]:
                 # Create a new string to describe the next part of the
@@ -19,13 +23,15 @@ def tree_to_disjunctive_normal(tree):
                 prefix = '' if curr_string == '(' else ' AND '
                 find_conditions(tree[attr][x], curr_string +
                         '{}{} = {}'.format(prefix, attr, x))
+    find_conditions.count = 0
     find_conditions(tree)
-    return ' OR\n'.join(conditions)
+    return (' OR\n'.join(conditions), find_conditions.count)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--decision_tree', required=True)
     parser.add_argument('-o', '--output', required=True)
+    parser.add_argument('-s', '--print_splits', action="store_true")
 
     args = parser.parse_args()
 
@@ -34,4 +40,6 @@ with open(args.decision_tree, 'rb') as t:
 
 with open(args.output, 'wb') as o:
     DNFString = tree_to_disjunctive_normal(tree)
-    o.write(DNFString)
+    if args.print_splits:
+        print DNFString[1]
+    o.write(DNFString[0])
